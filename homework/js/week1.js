1ac714be017ec946169cd46e2798de3447355f95
HaTa <ta.h@husky.neu.edu>
2020-01-15 23:18:50 -0500

    Add link to homework week 1 extra, photo filter version

--- homework/js/week1.js
+++ homework/js/week1.js
@@ -8,5 +8,18 @@ function draw(image) {
     let canvas = document.getElementById("mypic");
     let ctx = canvas.getContext("2d");
     ctx.drawImage(image, 0, 0);
-    let imageData = ctx
+    let imageData = ctx.getImageData(0, 0, 700, 525);
+    let data = imageData.data;
+
+    let grayscale = function() {
+        for (let i = 0; i < data.length; i+=4) {
+            let avg = (data[i] + data[i+1] + data[i+2]) / 3;
+            data[i] = avg;
+            data[i+1] = avg;
+            data[i+2] = avg;
+        }
+        ctx.putImageData(imageData, 0, 0);
+    }
+
+    document.getElementById("monochromeBtn").addEventListener("click", grayscale);
 }
