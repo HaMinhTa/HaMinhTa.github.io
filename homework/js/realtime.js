caf330ad98b4406814cf0f2f90742f84baaa4b08
HaTa <ta.h@husky.neu.edu>
2020-02-06 16:42:28 -0500

    add homework week 4

--- homework/js/realtime.js
+++ homework/js/realtime.js
@@ -4,13 +4,15 @@ var frequency = 1 * 1000; // 1 seconds
 var dataMax = 5;
 var data = [];
 
-var margin = {top: 50, right: 20, bottom: 100, left: 50};
+var margin = {top: 80, right: 50, bottom: 60, left: 50};
 var width = window.innerWidth - margin.left - margin.right;
 var height = window.innerHeight - margin.top - margin.bottom;
 
 var svg = d3.select("#chart")
-  .attr("width", width)
-  .attr("height", height);
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.bottom + margin.top)
+  .append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 var barWidth = width / dataMax / 2;
 
@@ -18,14 +20,12 @@ var x = d3.scaleLinear()
   .domain([dataMax, 1])
   .range([margin.left, width - barWidth]);
 
-var barHeight = d3.scaleLinear() 
-  .domain([0, d3.max(data)])
-  .range([height, margin.top]);
+var barHeight = d3.scaleLinear()
+  .range([height, 0]);
 
 var yAxis = svg.append("g")
   .attr("class", "axis")
   .attr("transform", `translate(${margin.left}, 0)`);
-
 
 function fetchData() {
 
@@ -69,8 +69,7 @@ function fetchData() {
         return height - barHeight(d.users);
       })
       .attr("y", function(d) {
-        var h = barHeight(d.users);
-        return h;
+        return barHeight(d.users);
       })
       .attr("x", function(d, i) {
         return x(i + 1);
@@ -82,8 +81,66 @@ function fetchData() {
       .attr("y", height)
       .remove();
 
-  });
 
+    var fontSize = 20;
+
+    var labels = svg.selectAll(".label")
+      .data(data, function(d) {
+        return d.timestamp;
+      });
+
+    var enterLabels = labels.enter().append("text")
+      .attr("class", "label")
+      .attr("font-size", fontSize)
+      .attr("fill", "white")
+      .attr("baseline-shift", "-100%")
+      .attr("y", height)
+      .attr("x", function(d, i) {
+        return x(i + 1);
+      })
+      .attr("width", barWidth);
+
+    labels.merge(enterLabels)
+      .transition().duration(frequency / 2)
+      .each(function(d,i) {
+        var textElement = d3.select(this);
+        textElement.text(" ");
+
+        var currLabel = d.users + " users on the site";
+        var words = currLabel.split(" ");
+
+        var tspan = textElement.append("tspan")
+          .attr("x", x(i+1))
+          .attr("y", barHeight(d.users))
+          .attr("baseline-shift", "-100%");
+        var line = 0;
+
+        words.forEach(function(word) {
+          var sentence = tspan.text();
+          tspan.text(sentence + " " + word);
+          var lineWidth = tspan.node().getBoundingClientRect().width;
+
+          if (lineWidth > barWidth) {
+            line++;
+            tspan.text(sentence);
+            tspan = textElement.append("tspan")
+              .attr("baseline-shift", "-100%")
+              .attr("y", barHeight(d.users) + (fontSize * line))
+              .attr("x", x(i + 1))
+              .text(word);
+
+          }
+        });
+      })
+      .attr("y", function(d) {
+        return barHeight(d.users);
+      })
+      .attr("x", function(d, i) {
+        return x(i + 1);
+      });
+
+    labels.exit().remove();
+  });
 }
 
 fetchData();
